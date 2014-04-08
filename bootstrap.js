68b580129c00ab01dd14af29b347ad2097155d69
Jeremy M. Poley <jpoley15@Jeremy-M-Poley.local>
2014-04-08 13:51:30 -0600

    Extension loading implemented

--- bootstrap.js
+++ bootstrap.js
@@ -2,10 +2,10 @@
 
 
 chrome.tabs.onUpdated.addListener(function(id, info, tab){
-  
+
     if (tab.url.toLowerCase().indexOf("google.com") !== -1){
       chrome.pageAction.show(tab.id);
+      chrome.tabs.executeScript(null, {"file": "extension.js"});
         }
 
-    //chrome.tabs.executeScript(null, {"file": "path/to/extension.js"});
 });
---
+++ extension.js
@@ -0,0 +1,3 @@
+
+
+console.log("Extension is loaded!")
