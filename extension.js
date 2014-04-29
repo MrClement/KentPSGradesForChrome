75e511a0273269bdc7d3c06eb8b64fc186583cb6
MrClement <aclement@kentdenver.org>
2014-04-29 13:20:13 -0600

    stuff

--- extension.js
+++ extension.js
@@ -20,9 +20,9 @@ function showOrHideCategories(){
     document.getElementsByName("weighted")[0].addEventListener("change", showOrHideCategories, false);
 
   } else {
-  htmlToInsert = '<form action=""><input type="checkbox" name="weighted" value="Weighted">Weighted by category?<br></form>';
-  newNode.innerHTML = htmlToInsert;
-  document.getElementsByName("weighted")[0].addEventListener("change", showOrHideCategories, false);
+    htmlToInsert = '<form action=""><input type="checkbox" name="weighted" value="Weighted">Weighted by category?<br></form>';
+    newNode.innerHTML = htmlToInsert;
+    document.getElementsByName("weighted")[0].addEventListener("change", showOrHideCategories, false);
   }
 }
 function reCalculate(){
@@ -50,7 +50,7 @@ function reCalculate(){
   }
   var newGrade = 0;
   for(var i = 0 ; i < categories.length; i++) {
-      newGrade += (categoryScores[i][0]/categoryScores[i][1])*categoryScores[i][2]/100;
+    newGrade += (categoryScores[i][0]/categoryScores[i][1])*categoryScores[i][2]/100;
   }
   newGrade *= 100;
   newGrade = parseFloat(newGrade).toFixed(2);
@@ -60,96 +60,96 @@ function reCalculate(){
 }
 function main() {
   var tables = document.getElementsByTagName("table");
-var header;
-var headerIndex;
-var finalHeader;
-var finalHeaderIndex;
-for(var i = 0 ; i < tables.length ; i++) {
-   if(tables[i].innerHTML.indexOf("Due Date") > -1) {
-     header = tables[i];
-     headerIndex = i;
-   } else if(tables[i].innerHTML.indexOf("Course") > -1) {
-     finalHeader = tables[i];
-     finalHeaderIndex = i;
-   }
-}
-var topRows = finalHeader.getElementsByTagName("tr");
-var topLocs = topRows[0].getElementsByTagName("th");
-var gradeIndex;
-for(var i = 0 ; i < topLocs.length ; i++){
-  if(topLocs[i].innerHTML.indexOf("Final Grade") > -1){
-  gradeIndex = i;
-  }
-}
-var gradeHTML = topRows[1].getElementsByTagName("td")[gradeIndex];
-finalGradeHTML = gradeHTML;
-var gradeText = gradeHTML.innerHTML;
-
-if(gradeText.indexOf("%") == -1) {
-
-categories = new Array();
-var rows = header.getElementsByTagName("tr");
-var locs = rows[0].getElementsByTagName("th");
-var locTitles = new Array();
-for(var i = 0 ; i < locs.length ; i++){
-  locTitles[i] = locs[i].innerHTML;
-}
-
-var scoreData = new Array();
-var categoryLoc = locTitles.indexOf("Category");
-var scoreLoc = locTitles.indexOf("Score");
-var totalPoints = 0;
-var earnedPoints = 0;
-
-for(var i = 1 ; i < rows.length ; i++) {
-  var parsedRows = rows[i].getElementsByTagName("td");
-  var singleScore = new Array();
-  singleScore[0] = parsedRows[categoryLoc].innerHTML;
-  if(categories.indexOf(singleScore[0]) == -1) {
-    categories.push(singleScore[0]);
-  }
-  var purple = parsedRows[scoreLoc+2].innerHTML;
-  var orange = parsedRows[scoreLoc+3].innerHTML;
-  if(purple == "" && orange == "" ) {
-    singleScore[1] = parsedRows[scoreLoc+4].getElementsByTagName("span")[0].innerHTML;
-    var as = parsedRows[scoreLoc+4].getElementsByTagName("span")[0].getElementsByTagName("a");
-    var newScore = singleScore[1].split("/");
-    if(as.length > 0) {
-      newScore[0] = newScore[0].substring(newScore[0].indexOf(">")+1, newScore[0].length-1);
-      earnedPoints += parseInt(newScore[0]);
-      singleScore[2] = parseInt(newScore[0]);
-      totalPoints += parseInt(newScore[2]);
-      singleScore[3] = parseInt(newScore[2]);
-    } else if(newScore[0].indexOf("-") == -1) {
-        earnedPoints += parseInt(newScore[0]);
-        singleScore[2] = parseInt(newScore[0]);
-        totalPoints += newScore.length == 1 ? 0 : parseInt(newScore[1]);
-        singleScore[3] = newScore.length == 1 ? 0 : parseInt(newScore[1]);
+  var header;
+  var headerIndex;
+  var finalHeader;
+  var finalHeaderIndex;
+  for(var i = 0 ; i < tables.length ; i++) {
+    if(tables[i].innerHTML.indexOf("Due Date") > -1) {
+      header = tables[i];
+      headerIndex = i;
+    } else if(tables[i].innerHTML.indexOf("Course") > -1) {
+      finalHeader = tables[i];
+      finalHeaderIndex = i;
     }
-    scoreData[i-1] = singleScore;
   }
-}
-var studentPercentage = earnedPoints / totalPoints *100;
-studentPercentage = parseFloat(studentPercentage).toFixed(2);
+  var topRows = finalHeader.getElementsByTagName("tr");
+  var topLocs = topRows[0].getElementsByTagName("th");
+  var gradeIndex;
+  for(var i = 0 ; i < topLocs.length ; i++){
+    if(topLocs[i].innerHTML.indexOf("Final Grade") > -1){
+      gradeIndex = i;
+    }
+  }
+  var gradeHTML = topRows[1].getElementsByTagName("td")[gradeIndex];
+  finalGradeHTML = gradeHTML;
+  var gradeText = gradeHTML.innerHTML;
 
-globalScoreData = scoreData;
-if(gradeText.indexOf("%") == -1) {
-  gradeHTML.innerHTML += "(" + studentPercentage + "%)";
-}
+  if(gradeText.indexOf("%") == -1) {
 
-var htmlToInsert = '<form action=""><input type="checkbox" name="weighted" value="Weighted">Weighted by category?<br></form>';
-var content = document.getElementById("content-main");
-var newNode = document.createElement("div");
-newNode.innerHTML = htmlToInsert;
-this.newNode = newNode;
+    categories = new Array();
+    var rows = header.getElementsByTagName("tr");
+    var locs = rows[0].getElementsByTagName("th");
+    var locTitles = new Array();
+    for(var i = 0 ; i < locs.length ; i++){
+      locTitles[i] = locs[i].innerHTML;
+    }
 
-var target = content.getElementsByTagName("table")[1];
-content.insertBefore(newNode, target);
-document.getElementsByName("weighted")[0].addEventListener("change", showOrHideCategories, false);
+    var scoreData = new Array();
+    var categoryLoc = locTitles.indexOf("Category");
+    var scoreLoc = locTitles.indexOf("Score");
+    var totalPoints = 0;
+    var earnedPoints = 0;
+
+    for(var i = 1 ; i < rows.length ; i++) {
+      var parsedRows = rows[i].getElementsByTagName("td");
+      var singleScore = new Array();
+      singleScore[0] = parsedRows[categoryLoc].innerHTML;
+      if(categories.indexOf(singleScore[0]) == -1) {
+        categories.push(singleScore[0]);
+      }
+      var purple = parsedRows[scoreLoc+2].innerHTML;
+      var orange = parsedRows[scoreLoc+3].innerHTML;
+      if(purple == "" && orange == "" ) {
+        singleScore[1] = parsedRows[scoreLoc+4].getElementsByTagName("span")[0].innerHTML;
+        var as = parsedRows[scoreLoc+4].getElementsByTagName("span")[0].getElementsByTagName("a");
+        var newScore = singleScore[1].split("/");
+        if(as.length > 0) {
+          newScore[0] = newScore[0].substring(newScore[0].indexOf(">")+1, newScore[0].length-1);
+          earnedPoints += parseInt(newScore[0]);
+          singleScore[2] = parseInt(newScore[0]);
+          totalPoints += parseInt(newScore[2]);
+          singleScore[3] = parseInt(newScore[2]);
+        } else if(newScore[0].indexOf("-") == -1) {
+          earnedPoints += parseInt(newScore[0]);
+          singleScore[2] = parseInt(newScore[0]);
+          totalPoints += newScore.length == 1 ? 0 : parseInt(newScore[1]);
+          singleScore[3] = newScore.length == 1 ? 0 : parseInt(newScore[1]);
+        }
+        scoreData[i-1] = singleScore;
+      }
+    }
+    var studentPercentage = earnedPoints / totalPoints *100;
+    studentPercentage = parseFloat(studentPercentage).toFixed(2);
+
+    globalScoreData = scoreData;
+    if(gradeText.indexOf("%") == -1) {
+      gradeHTML.innerHTML += "(" + studentPercentage + "%)";
+    }
+
+    var htmlToInsert = '<form action=""><input type="checkbox" name="weighted" value="Weighted">Weighted by category?<br></form>';
+    var content = document.getElementById("content-main");
+    var newNode = document.createElement("div");
+    newNode.innerHTML = htmlToInsert;
+    this.newNode = newNode;
+
+    var target = content.getElementsByTagName("table")[1];
+    content.insertBefore(newNode, target);
+    document.getElementsByName("weighted")[0].addEventListener("change", showOrHideCategories, false);
 
 
-console.log("Extension is loaded!");
-}
+    console.log("Extension is loaded!");
+  }
 }
 
 main();
