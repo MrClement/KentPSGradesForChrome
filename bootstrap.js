e84e4ed586944239d8e41c8243269326a9207709
MrClement <aclement@kentdenver.org>
2014-05-20 16:37:47 -0600

    Working towards jquery

--- bootstrap.js
+++ bootstrap.js
@@ -3,6 +3,6 @@ chrome.tabs.onUpdated.addListener(function(id, info, tab){
     if (tab.url.toLowerCase().indexOf("powerschool.kentdenver.org") != -1){
       chrome.pageAction.show(tab.id);
       chrome.tabs.executeScript(null, {"file": "extension.js"});
-        }
+    }
 
 });
