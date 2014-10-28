f7537aa200a54603c5e81613b3fe82ebba7e569d
Alex Clement <aclement@kentdenver.org>
2014-10-28 16:04:48 -0600

    Update bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -3,7 +3,7 @@ chrome.tabs.onUpdated.addListener(function(id, info, tab){
     if (url.indexOf("powerschool.kentdenver.org") != -1){
       chrome.pageAction.show(tab.id);
       if(url.indexOf("home.html") != -1) {
-        chrome.tabs.executeScript(null, {"file": "mainpage.js"});
+        //chrome.tabs.executeScript(null, {"file": "mainpage.js"});
       } else if(url.indexOf("scores") != -1 ) {
         chrome.tabs.executeScript(null, {"file": "extension.js"});
 
